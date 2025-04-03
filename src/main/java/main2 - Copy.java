104f5b9e0f366d1e607609310e934840d229d6e4
rohanmule0405 <rohanmule0405@gmail.com>
2025-04-04 00:11:55 +0530

    add this files

---
+++ src/main/java/main2 - Copy.java
@@ -0,0 +1,14 @@
+
+public class main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("This is second file ");
+ System.out.println("This is  second file of second line");
+ 
+ System.out.println("This is second file ");
+ System.out.println("This is  second file of second line");
+ 
+	}
+
+}
---
+++ src/main/java/mainn3 - Copy - Copy.java
@@ -0,0 +1,8 @@
+
+public class mainn3 {
+	public static void main(String args[]) {
+		System.out.println("asdfg");
+		System.out.println("Asdfdfn");
+	}
+
+}
