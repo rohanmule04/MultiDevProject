6dce393cd211b9976818941e187142fb5866115d
rohanmule0405 <rohanmule0405@gmail.com>
2025-04-03 23:13:16 +0530

    add rohan.java

---
+++ src/main/java/rohan.java
@@ -0,0 +1,10 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Multi-Developer Collaboration!");
+		System.out.println("asdfghjkl");
+	}
+
+}
